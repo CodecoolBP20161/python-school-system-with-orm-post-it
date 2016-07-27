8a5b401c0737f9dc963d3ba9f9e200077b9857f5
Kristof Galla <gallakristof@gmail.com>
2016-07-27 13:07:07 +0200

    Create basic models and fill with example data

--- example_data.py
+++ example_data.py
@@ -1,3 +1,39 @@
 # This script can generate example data for "City" and "InterviewSlot" models.
-
+import random
+import datetime
 from models import *
+
+
+# ccbudapest = School.create(school='Budapest school')
+# ccmiskolc = School.create(school='Miskolc school')
+# cckrakow = School.create(school='Krakow school')
+
+
+schools = [{'school': 'Budapest school'},
+           {'school': 'Miskolc school'},
+           {'school': 'Krakow school'}]
+
+for school in schools:
+    s = School(**school)
+    s.save()
+
+applicants = [{
+    'first_name': "Levente",
+    'last_name': "Csanyi",
+    'application_code': 'SCRD4',
+    'status': 'new',
+    'application_time': datetime.date(1988, 12, 1)},
+    {
+        'first_name': "Lvnt",
+        'last_name': "Cny",
+        'application_code': random.randint(0,4555),
+        'status': 'new',
+        'application_time': datetime.date(1928, 12, 1)}
+]
+
+for applicant in applicants:
+    a = Applicant(**applicant)
+    a.save()
+#
+# cities = [{'':}]
+
--- models.py
+++ models.py
@@ -3,10 +3,33 @@ from peewee import *
 # Configure your database connection here
 # database name = should be your username on your laptop
 # database user = should be your username on your laptop
-db = PostgresqlDatabase('dbname', user='dbuser')
+db = PostgresqlDatabase('krs', user='krs')
 
 
 class BaseModel(Model):
     """A base model that will use our Postgresql database"""
     class Meta:
         database = db
+
+
+class Applicant(BaseModel):
+    first_name = CharField()
+    last_name = CharField()
+    application_code = CharField(null=False)
+    status = CharField()
+    # closest_school = ForeignKeyField(School)
+    application_time = DateField()
+
+
+
+class City(BaseModel):
+    city = CharField()
+    # closest_school = ForeignKeyField(School)
+
+
+
+class School(BaseModel):
+    school = CharField()
+
+
+
